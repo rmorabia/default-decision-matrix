0ec9e439b6c386987cfc4ec5472037809033f6f6
Radhika Morabia <hi@rmorabia.com>
2019-04-10 21:17:59 -0400

    My object was wrong!
    EXCUSE THIS.

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const PE = document.querySelector("#PE")
 const EE = document.querySelector("#EE")
 
 const results = {
-  "000": "You should listen to fun podcasts or audiobooks!",
-  "001": "You should work on some chores.",
-  "010": "You should watch some TV or go on YouTube.",
-  "100": "You should listen to some programming-related podcasts.",
-  "110": "You should read an Instapaper article or a graphic novel.",
-  "101": "You should listen to programming podcasts or do some chores.",
-  "011": "You should practice your mandolin!",
-  "111": "You should watch a programming talk or tutorial on YouTube."
+  '000': 'You should listen to fun podcasts or audiobooks!',
+  '001': 'You should watch some TV or go on YouTube.',
+  '010': 'You should do some chores.',
+  '100': 'You should listen to some programming-related podcasts.',
+  '110': 'You should listen to programming podcasts or do some chores.',
+  '101': 'You should read an Instapaper article or a graphic novel.',
+  '011': 'You should practice your mandolin!',
+  '111': 'You should watch a programming talk or tutorial on YouTube.'
 }
 
 button.addEventListener("click", function() {
